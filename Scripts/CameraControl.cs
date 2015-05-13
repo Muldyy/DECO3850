36b0f715c3c3dc32370b81b50bfa39119178d80a
En Bo <enbo66@hotmail.com>
2015-05-13 14:57:17 +1000

    Adding in all the script files for the project

---
+++ Scripts/CameraControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    public Camera mainCamera;
+
+    private float viewScale;
+    private float xPos;
+    private float yPos;
+
+	// Use this for initialization
+	void Start () {
+        xPos = 0f;
+        yPos = 0.5f;
+        viewScale = 3.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(xPos);
+        //Debug.Log(yPos);
+        //Debug.Log(viewScale);
+        mainCamera.orthographicSize = viewScale;
+        mainCamera.transform.position = new Vector3(xPos, yPos, -10);
+	}
+
+    public void zoomIn()
+    {
+        viewScale -= 0.1f;
+    }
+
+    public void zoomOut()
+    {
+        viewScale += 0.1f;
+    }
+
+    public void moveUp()
+    {
+        yPos -= 0.1f;
+    }
+
+    public void moveDown()
+    {
+        yPos += 0.1f;
+    }
+
+    public void moveLeft()
+    {
+        xPos += 0.1f;
+    }
+
+    public void moveRight()
+    {
+        xPos -= 0.1f;
+    }
+
+}
---
+++ Scripts/SandPitControls.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandPitControls : MonoBehaviour {
+
+    Renderer renderer;
+
+    private float offsetX = 0.31f;
+    private float offsetY = 0.13f;
+
+    private float scaleX = 0.5500004f;
+    private float scaleY = 0.5600004f;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void minusOffSetX()
+    {
+        offsetX -= 0.01f;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+
+    public void plusOffSetX()
+    {
+        offsetX += 0.01f;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+
+    
+    public void minusOffsetY()
+    {
+        offsetY -= 0.01f;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+
+    public void plusOffSetY()
+    {
+        offsetY += 0.01f;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+    
+    public void minusScaleX()
+    {
+        scaleX -= 0.01f;
+        renderer.material.mainTextureScale = new Vector2 (scaleX, scaleY);
+    }
+
+    public void plusScaleX()
+    {
+        scaleX += 0.01f;
+        renderer.material.mainTextureScale = new Vector2 (scaleX, scaleY);
+    }
+
+    public void minusScaleY()
+    {
+        scaleY -= 0.01f;
+        renderer.material.mainTextureScale = new Vector2 (scaleX, scaleY);
+    }
+
+    public void plusScaleY()
+    {
+        scaleY += 0.01f;
+        renderer.material.mainTextureScale = new Vector2 (scaleX, scaleY);
+    }
+}
