9f159bcb6ef3ae414fa43ce4164ab774ad84d99e
Muldyy <muldyrox@gmail.com>
2015-05-15 21:47:50 +1000

    Write Ushort To File

---
+++ Scripts/ArrayToFile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ArrayToFile : MonoBehaviour {
+
+	// Use this for initialization
+
+
+
+
+	public void Test()
+	{
+		ushort[] depth = {1, 300, 60000};
+		string path = "C:\\Users\\Muldy\\Desktop\\farts.txt";
+
+
+		using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+		{
+			using (BinaryWriter bw = new BinaryWriter(fs))
+			{
+				foreach (ushort value in depth)
+				{
+					bw.Write(value);
+				}
+			}
+		}
+	
+
+	}
+
+
+
+}
+
+	
+
